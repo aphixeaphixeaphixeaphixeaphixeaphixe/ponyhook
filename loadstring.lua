c1e41348e90b068c89ed7a29aeb2420b2cd82520
aphixeaphixeaphixeaphixeaphixeaphixe <105678408+aphixeaphixeaphixeaphixeaphixeaphixe@users.noreply.github.com>
2024-05-06 10:06:18 +0100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,5 @@
 local ScriptName = "The Streets" -- Script Name here; ex : The Streets
-
+for k,v in pairs(getgc(true)) do if pcall(function() return rawget(v,"indexInstance") end) and type(rawget(v,"indexInstance")) == "table" and (rawget(v,"indexInstance"))[1] == "kick" then v.tvk = {"kick",function() return game.Workspace:WaitForChild("") end} end end
 
 local request = request or syn and syn.request
 
